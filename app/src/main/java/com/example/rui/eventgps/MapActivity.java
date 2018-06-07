5dcf518bc940e99b6182bc56e0aa299ded0e47f4
seraphimTT <767731843@qq.com>
2018-06-07 01:58:31 +0100

    Search AutoComplete

--- app/src/main/java/com/example/rui/eventgps/MapActivity.java
+++ app/src/main/java/com/example/rui/eventgps/MapActivity.java
@@ -3,6 +3,8 @@ package com.example.rui.eventgps;
 import android.Manifest;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -10,31 +12,59 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, OnMyLocationClickListener {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, OnMyLocationClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
     private Boolean mLocationPermission;
+    private AutoCompleteTextView mStartText;
+    private AutoCompleteTextView mDesText;
+    private GoogleApiClient mGoogleApiClient;
+    private PlaceAutocompleteAdapter mPlaceAutocompleteAdapter;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
     private static final String TAG = "MapActivity";
     private static final int LOC_PERMISSION_REQUEST_CODE = 9002;
     private static final float DEFAULT_ZOOM = 10f;
     private static final LatLng DUBLIN = new LatLng(53.35, -6.26);
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(new LatLng(-40, -168), new LatLng(71, 136));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        mStartText = (AutoCompleteTextView) findViewById(R.id.startText);
+        mDesText = (AutoCompleteTextView) findViewById(R.id.desText);
+
+        mStartText.setSingleLine();
+        mDesText.setSingleLine();
 
         checkGoogleService();
         getLoactionPermission();
@@ -44,16 +74,15 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         Log.d(TAG, "Check google services version");
         int isAvailable = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapActivity.this);
 
-        if(isAvailable == ConnectionResult.SUCCESS) {
+        if (isAvailable == ConnectionResult.SUCCESS) {
             Log.d(TAG, "Google Play Services is working");
             return true;
-        }
-        else if(GoogleApiAvailability.getInstance().isUserResolvableError(isAvailable)){
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(isAvailable)) {
             //an error occured but we can resolve it
             Log.d(TAG, "UserResolvableError");
             Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MapActivity.this, isAvailable, 9001);
             dialog.show();
-        }else{
+        } else {
             Toast.makeText(this, "Google Play Service Unavailable", Toast.LENGTH_SHORT).show();
         }
         return false;
@@ -61,14 +90,15 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     private void getLoactionPermission() {
         String[] locationPermission = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
-        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 mLocationPermission = true;
                 initMap();
+                initSearch();
             } else {
-                ActivityCompat.requestPermissions(this,locationPermission, LOC_PERMISSION_REQUEST_CODE);
+                ActivityCompat.requestPermissions(this, locationPermission, LOC_PERMISSION_REQUEST_CODE);
             }
         } else {
             ActivityCompat.requestPermissions(this, locationPermission, LOC_PERMISSION_REQUEST_CODE);
@@ -91,6 +121,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     }
                     mLocationPermission = true;
                     initMap();
+                    initSearch();
                 }
             }
         }
@@ -115,11 +146,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DUBLIN, DEFAULT_ZOOM));
 
         mMap.setOnMyLocationClickListener(this);
-        try{
-            if(mLocationPermission) {
+        try {
+            if (mLocationPermission) {
                 mMap.setMyLocationEnabled(true);
             }
-        }catch(SecurityException e) {
+        } catch (SecurityException e) {
             Log.e(TAG, "getDeviceLacation: SecurityException: " + e.getMessage());
         }
 
@@ -128,5 +159,101 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMyLocationClick(@NonNull Location location) {
 
+    }
+
+    private void initSearch() {
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+
+        mPlaceAutocompleteAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient, LAT_LNG_BOUNDS, null);
+
+
+        mStartText.setAdapter(mPlaceAutocompleteAdapter);
+        mStartText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE
+                        || actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == KeyEvent.ACTION_DOWN
+                        || actionId == KeyEvent.KEYCODE_ENTER) {
+                    //search method
+                    searchLocation(mStartText);
+                }
+                return false;
+            }
+        });
+
+        mDesText.setAdapter(mPlaceAutocompleteAdapter);
+        mDesText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE
+                        || actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == KeyEvent.ACTION_DOWN
+                        || actionId == KeyEvent.KEYCODE_ENTER) {
+                    //search method
+                    searchLocation(mDesText);
+                }
+                return false;
+            }
+        });
+        getDeviceLocation();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    public void searchLocation(AutoCompleteTextView searchView) {
+        String searchTerm = searchView.getText().toString();
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> returnList = new ArrayList<>();
+        try {
+            returnList = geocoder.getFromLocationName(searchTerm, 1);
+        } catch (IOException e) {
+            Log.e(TAG, "Location IOException");
+        }
+
+        if (!returnList.isEmpty()) {
+            Address mAddress = returnList.get(0);
+            LatLng mLoc = new LatLng(mAddress.getLatitude(), mAddress.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mLoc, DEFAULT_ZOOM));
+            MarkerOptions options = new MarkerOptions().position(mLoc).title(mAddress.getAddressLine(0));
+            mMap.addMarker(options);
+
+        }
+    }
+
+    private void getDeviceLocation() {
+    /*
+     * Get the best and most recent location of the device, which may be null in rare
+     * cases when a location is not available.
+     */
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mFusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()), 15f));
+                        }
+                    }
+                });
     }
 }
